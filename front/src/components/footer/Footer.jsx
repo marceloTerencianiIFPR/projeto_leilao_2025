714d43a5a4f6ef17e97be7e23abb3879f7527bcb
Frank <frankwco@gmail.com>
2025-06-09 19:18:41 -0300

    primeiro

---
+++ front/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <>
+            <div className="footer">
+                &copy; todos os direitos reservados
+            </div>
+        </>
+    );
+}
+export default Footer;
